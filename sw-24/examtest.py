18fcc91b882e2d44066b11368b2dce9eccc3d443
ECHAMBON <ewen.chambonneau.ext@allianz-trade.com>
2025-01-09 19:33:26 +0100

    Final version

--- sw-24/examtest.py
+++ sw-24/examtest.py
@@ -2,19 +2,26 @@ import unittest
 import math
 
 
+def sqrt(val):
+    if not isinstance(val, int):
+        return TypeError
+    if val < 0:
+        return ValueError
+    return math.sqrt(val)
+
+
 class TestSqrt(unittest.TestCase):
     def test_sqrt(self):
-        self.assertEqual(math.sqrt(36), 6)  # Test for positive numbers
-        self.assertEqual(math.sqrt(16), 4)
-        self.assertEqual(math.sqrt(9), 3)
-        self.assertEqual(math.sqrt(0), 0)  # Test for 0
-        self.assertAlmostEqual(math.sqrt(2), 1.41421356, places=8)
+        self.assertEqual(sqrt(36), 6)  # Test for positive numbers
+        self.assertEqual(sqrt(16), 4)
+        self.assertEqual(sqrt(9), 3)
+        self.assertEqual(sqrt(0), 0)  # Test for 0
+        self.assertAlmostEqual(sqrt(2), 1.41421356, places=8)
         # Test for non integrer number
-        with self.assertRaises(ValueError):  # Test for negative number
-            math.sqrt(-1)
-        with self.assertRaises(TypeError):
-            # Test for a value different than a number
-            math.sqrt("a")
+        self.assertEqual(sqrt(-1), ValueError)
+        # Test for negative number
+        self.assertEqual(sqrt("a"), TypeError)
+        # Test for a value different than a number
 
 
 if __name__ == '__main__':
